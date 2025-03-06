332164eab9e5f678d9d0465d83dc2615f839aed0
Vijay-Nataraj <victoryvijayn@gmail.com>
2025-03-06 18:47:54 +0530

    baseURL added

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3000/api/v1";
+const baseURL = "https://project-be-xtyb.onrender.com/api/v1";
 
 const instance = axios.create({
   baseURL,
